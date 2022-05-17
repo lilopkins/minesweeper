944b801adc6d7f1650fbbc4389562172de9406a0
Lily Hopkins <lily@hpkns.uk>
2022-05-17 13:06:28 +0100

    Added JavaFX

---
+++ src/main/java/uk/hpkns/minesweeper/GUIGame.java
@@ -0,0 +1,22 @@
+package uk.hpkns.minesweeper;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUIGame extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label l = new Label("Minesweeper!");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void startGame(int size) {
+        launch();
+    }
+}
--- src/main/java/uk/hpkns/minesweeper/Main.java
+++ src/main/java/uk/hpkns/minesweeper/Main.java
@@ -3,6 +3,10 @@ package uk.hpkns.minesweeper;
 public class Main {
 
     public static void main(String[] args) {
-        TerminalGame.start(10);
+        if (System.getenv().containsKey("TERM")) {
+            TerminalGame.start(10);
+        } else {
+            GUIGame.startGame(10);
+        }
     }
 }
